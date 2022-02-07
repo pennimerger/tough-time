ea15fa056c9c88a7d436af70697c43da4184ab6f
Emmanuel Ashiegbulam <pennimerger@gmail.com>
2022-02-07 22:46:14 +0100

    Submitting tasks

---
+++ task.py
@@ -0,0 +1,35 @@
+#converting celsius to farenheit
+celsius=20
+farenheit=(celsius*1.8)+32
+#print(f'The weather is {celsius}\N{DEGREE SIGN}C in UK but {farenheit}\N{DEGREE SIGN}F in the US and they tell the same temperature.')
+
+#appending (...) to every number a user inputs.
+#user_0=float(input())
+#print(f'{user_0}...')
+
+#fix this list and print
+names=["Priscilla","Mummy G.O","Benedict","Dima",
+"Kingsley","Mic"]
+print(f'\n{names}')
+
+#What is the index of c++ ?
+programming_languages=['python','javascript','C','pascal',
+'c++','php','visual basic']
+print(f'\n{programming_languages}')
+print(f"c++ is indexed 4 as shown by this call - {programming_languages[4]}")
+
+#print python and php
+print(f'\n{programming_languages[0]}')
+print(programming_languages[5])
+
+#fix this list, append your name to this list and print
+name=["Priscilla","Mummy G.O","Benedict","Dima",
+"Kingsley","Mic"]
+name.append("pennimerger")
+print(f'\n{name}')
+
+#delete pascal from this list and print
+_programming_languages=['python','javascript','C','pascal',
+'c++','php','visual basic']
+del _programming_languages[3]
+print(f'\n{_programming_languages}')
